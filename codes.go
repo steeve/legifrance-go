c695d00be5507921e75a9c7e8cf79b376006ac38
Steeve Morin <steeve.morin@gmail.com>
2015-04-04 17:13:57 +0200

    Add French codes
    Signed-off-by: Steeve Morin <steeve.morin@gmail.com>

---
+++ codes.go
@@ -0,0 +1,151 @@
+package legifrance
+
+const (
+	CodeDeLArtisanat                                                                               = "LEGITEXT000006075116"
+	CodeDesAssurances                                                                              = "LEGITEXT000006073984"
+	CodeDeLAviationCivile                                                                          = "LEGITEXT000006074234"
+	CodeDuCinemaEtDeLImageAnimee                                                                   = "LEGITEXT000020908868"
+	CodeCivil                                                                                      = "LEGITEXT000006070721"
+	CodeDeCommerce                                                                                 = "LEGITEXT000005634379"
+	CodeDesCommunes                                                                                = "LEGITEXT000006070162"
+	CodeDesCommunesEtDeLaNouvelleCaledonie                                                         = "LEGITEXT000006070300"
+	CodeDeLaConsommation                                                                           = "LEGITEXT000006069565"
+	CodeDeLaConstructionEtDeLHabitation                                                            = "LEGITEXT000006074096"
+	CodeDeLaDefense                                                                                = "LEGITEXT000006071307"
+	CodeDeDeontologieDesArchitectes                                                                = "LEGITEXT000006074232"
+	CodeDisciplinaireEtPenalDeLaMarineMarchande                                                    = "LEGITEXT000006071188"
+	CodeDuDomaineDeLEtat                                                                           = "LEGITEXT000006070208"
+	CodeDuDomaineDeLEtatEtDesCollectivitesPubliquesAppliquableALaCollectiviteTerritorialeDeMayotte = "LEGITEXT000006074235"
+	CodeDuDomainePublicFluvialEtDeLaNavigationInterieure                                           = "LEGITEXT000006074237"
+	CodeDesDouanes                                                                                 = "LEGITEXT000006071570"
+	CodeDesDouanesDeMayotte                                                                        = "LEGITEXT000006071645"
+	CodeDeLEducation                                                                               = "LEGITEXT000006071191"
+	CodeElectoral                                                                                  = "LEGITEXT000006070239"
+	CodeDeLEnergie                                                                                 = "LEGITEXT000023983208"
+	CodeDeLEntreeEtDuSejourDesEtrangersEtDuDroitDAsile                                             = "LEGITEXT000006070158"
+	CodeDeLEnvironnement                                                                           = "LEGITEXT000006074220"
+	CodeDeLExpropriationPourCauseDUtilitePublique                                                  = "LEGITEXT000006074224"
+	CodeDeLaFamilleEtDeLAideSociale                                                                = "LEGITEXT000006072637"
+	CodeForestierNouveau                                                                           = "LEGITEXT000025244092"
+	CodeGeneralDeLaProprieteDesPersonnesPubliques                                                  = "LEGITEXT000006070299"
+	CodeGeneralDesCollectivitesTerritoriales                                                       = "LEGITEXT000006070633"
+	CodeGeneralDesImpots                                                                           = "LEGITEXT000006069577"
+	CodeGeneralDesImpotsAnnexe1                                                                    = "LEGITEXT000006069568"
+	CodeGeneralDesImpotsAnnexe2                                                                    = "LEGITEXT000006069569"
+	CodeGeneralDesImpotsAnnexe3                                                                    = "LEGITEXT000006069574"
+	CodeGeneralDesImpotsAnnexe4                                                                    = "LEGITEXT000006069576"
+	CodeDesInstrumentsMonetairesEtDesMedailles                                                     = "LEGITEXT000006070666"
+	CodeDesJuridictionsFinancieres                                                                 = "LEGITEXT000006070249"
+	CodeDeJusticeAdministrative                                                                    = "LEGITEXT000006070933"
+	CodeDeJusticeMilitaireNouveau                                                                  = "LEGITEXT000006071360"
+	CodeDeLaLegionDHonneurEtDeLaMedailleMilitaire                                                  = "LEGITEXT000006071007"
+	LivreDesProceduresFiscales                                                                     = "LEGITEXT000006069583"
+	CodeDesMarchesPublics2006                                                                      = "LEGITEXT000005627819"
+	CodeMinier                                                                                     = "LEGITEXT000006071785"
+	CodeMinierNouveau                                                                              = "LEGITEXT000023501962"
+	CodeMonetaireEtFinancier                                                                       = "LEGITEXT000006072026"
+	CodeDeLaMutualite                                                                              = "LEGITEXT000006074067"
+	CodeDeLOrganisationJudiciare                                                                   = "LEGITEXT000006071164"
+	CodeDuPatrimoine                                                                               = "LEGITEXT000006074236"
+	CodePenal                                                                                      = "LEGITEXT000006070719"
+	CodeDesPensionsCivilesEtMilitairesDeRetraite                                                   = "LEGITEXT000006070302"
+	CodeDesPensionsDeRetraiteDesMarinsFrancaisDuCommerceDePecheOuDePlaisance                       = "LEGITEXT000006074066"
+	CodeDesPensionsMilitairesDInvaliditeEtDesVictimesDeLaGuerre                                    = "LEGITEXT000006074068"
+	CodeDesPortsMaritimes                                                                          = "LEGITEXT000006074233"
+	CodeDesPostesEtDesCommunicationsElectroniques                                                  = "LEGITEXT000006070987"
+	CodeDeProcedureCivile                                                                          = "LEGITEXT000006070716"
+	CodeDeProcedurePenale                                                                          = "LEGITEXT000006071154"
+	CodeDesProceduresCivilesDExecution                                                             = "LEGITEXT000025024948"
+	CodeDeLaProprieteIntellectuelle                                                                = "LEGITEXT000006069414"
+	CodeDeLaRecherche                                                                              = "LEGITEXT000006071190"
+	CodeDeLaRoute                                                                                  = "LEGITEXT000006074228"
+	CodeRuralAncien                                                                                = "LEGITEXT000006071366"
+	CodeRuralEtDeLaPecheMaritime                                                                   = "LEGITEXT000006071367"
+	CodeDeLaSantePublique                                                                          = "LEGITEXT000006072665"
+	CodeDeLaSecuriteInterieure                                                                     = "LEGITEXT000025503132"
+	CodeDeLaSecuriteSociale                                                                        = "LEGITEXT000006073189"
+	CodeDuServiceNational                                                                          = "LEGITEXT000006071335"
+	CodeDuSport                                                                                    = "LEGITEXT000006071318"
+	CodeDuTourisme                                                                                 = "LEGITEXT000006074073"
+	CodeDesTransports                                                                              = "LEGITEXT000023086525"
+	CodeDuTravail                                                                                  = "LEGITEXT000006072050"
+	CodeDuTravailApplicableAMayotte                                                                = "LEGITEXT000006072052"
+	CodeDuTravailMaritime                                                                          = "LEGITEXT000006072051"
+	CodeDeLUrbanisme                                                                               = "LEGITEXT000006074075"
+	CodeDeLaVoirieRoutiere                                                                         = "LEGITEXT000006070667"
+)
+
+var Codes = []string{
+	CodeDeLArtisanat,
+	CodeDesAssurances,
+	CodeDeLAviationCivile,
+	CodeDuCinemaEtDeLImageAnimee,
+	CodeCivil,
+	CodeDeCommerce,
+	CodeDesCommunes,
+	CodeDesCommunesEtDeLaNouvelleCaledonie,
+	CodeDeLaConsommation,
+	CodeDeLaConstructionEtDeLHabitation,
+	CodeDeLaDefense,
+	CodeDeDeontologieDesArchitectes,
+	CodeDisciplinaireEtPenalDeLaMarineMarchande,
+	CodeDuDomaineDeLEtat,
+	CodeDuDomaineDeLEtatEtDesCollectivitesPubliquesAppliquableALaCollectiviteTerritorialeDeMayotte,
+	CodeDuDomainePublicFluvialEtDeLaNavigationInterieure,
+	CodeDesDouanes,
+	CodeDesDouanesDeMayotte,
+	CodeDeLEducation,
+	CodeElectoral,
+	CodeDeLEnergie,
+	CodeDeLEntreeEtDuSejourDesEtrangersEtDuDroitDAsile,
+	CodeDeLEnvironnement,
+	CodeDeLExpropriationPourCauseDUtilitePublique,
+	CodeDeLaFamilleEtDeLAideSociale,
+	CodeForestierNouveau,
+	CodeGeneralDeLaProprieteDesPersonnesPubliques,
+	CodeGeneralDesCollectivitesTerritoriales,
+	CodeGeneralDesImpots,
+	CodeGeneralDesImpotsAnnexe1,
+	CodeGeneralDesImpotsAnnexe2,
+	CodeGeneralDesImpotsAnnexe3,
+	CodeGeneralDesImpotsAnnexe4,
+	CodeDesInstrumentsMonetairesEtDesMedailles,
+	CodeDesJuridictionsFinancieres,
+	CodeDeJusticeAdministrative,
+	CodeDeJusticeMilitaireNouveau,
+	CodeDeLaLegionDHonneurEtDeLaMedailleMilitaire,
+	LivreDesProceduresFiscales,
+	CodeDesMarchesPublics2006,
+	CodeMinier,
+	CodeMinierNouveau,
+	CodeMonetaireEtFinancier,
+	CodeDeLaMutualite,
+	CodeDeLOrganisationJudiciare,
+	CodeDuPatrimoine,
+	CodePenal,
+	CodeDesPensionsCivilesEtMilitairesDeRetraite,
+	CodeDesPensionsDeRetraiteDesMarinsFrancaisDuCommerceDePecheOuDePlaisance,
+	CodeDesPensionsMilitairesDInvaliditeEtDesVictimesDeLaGuerre,
+	CodeDesPortsMaritimes,
+	CodeDesPostesEtDesCommunicationsElectroniques,
+	CodeDeProcedureCivile,
+	CodeDeProcedurePenale,
+	CodeDesProceduresCivilesDExecution,
+	CodeDeLaProprieteIntellectuelle,
+	CodeDeLaRecherche,
+	CodeDeLaRoute,
+	CodeRuralAncien,
+	CodeRuralEtDeLaPecheMaritime,
+	CodeDeLaSantePublique,
+	CodeDeLaSecuriteInterieure,
+	CodeDeLaSecuriteSociale,
+	CodeDuServiceNational,
+	CodeDuSport,
+	CodeDuTourisme,
+	CodeDesTransports,
+	CodeDuTravail,
+	CodeDuTravailApplicableAMayotte,
+	CodeDuTravailMaritime,
+	CodeDeLUrbanisme,
+	CodeDeLaVoirieRoutiere,
+}
